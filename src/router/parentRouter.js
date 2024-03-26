7ee801a327e6ba8c90d5fb66e82438f009755d10
Akshayan <takshayan@isa.ae>
2024-03-26 17:40:22 +0530

    feat: userNavbar addition

--- src/router/parentRouter.js
+++ src/router/parentRouter.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import AdminRouter from '../components/admin/AdminRouter';
-import AuthRouter from '../components/auth/AuthRouter';
-// import NotFound from './NotFound'; // Assuming NotFound component exists
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import AdminRouter from "../components/admin/AdminRouter";
+import AuthRouter from "../components/auth/AuthRouter";
+import UserRouter from "../components/user/userRouter";
 
-const ParentRouter = () =>{
+const ParentRouter = () => {
   return (
-      <Routes>
-        <Route path="/admin/*" element={<AdminRouter />} />
-        <Route path="/auth/*" element={<AuthRouter />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
-      </Routes>
+    <Routes>
+      <Route path="/admin/*" element={<AdminRouter />} />
+      <Route path="/auth/*" element={<AuthRouter />} />
+      <Route path="/user/*" element={<UserRouter />} />
+    </Routes>
   );
-}
+};
 export default ParentRouter;

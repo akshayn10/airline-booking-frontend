5566cc1dc9fbd3a504c1883542c7869efe69a8a4
Akshayan <takshayan@isa.ae>
2024-03-26 11:58:09 +0530

    feat: Reset password form addition

--- src/components/auth/AuthRouter.js
+++ src/components/auth/AuthRouter.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./login/login.js";
 import Signup from "./signup/signup.js";
 import ForgotPassword from "./login/forgotPassword/forgotPassword.js";
+import ResetPassword from "./login/resetPassword/resetPassword.js";
 
 const AuthRouter = () => {
   return (
     <Routes>
       <Route path="login" element={<Login />} />
       <Route path="forgot-password" element={<ForgotPassword />} />
+      <Route path="reset-password" element={<ResetPassword />} />
       <Route path="signup" element={<Signup />} />
     </Routes>
   );

99924612240fecce9f18569b4fcb3778e9b84e4c
Akshayan <takshayan@isa.ae>
2024-03-29 12:29:31 +0530

    feat: User Dashboard tables addition

---
+++ src/components/user/dashboard/upcomingTrips/upcomingTripsTable/upcomingTripsTable.js
@@ -0,0 +1,59 @@
+import { Space, Table} from "antd";
+import styles from "./upcomingTripsTable.module.css";
+
+
+const upcomingTripsColumns = [
+  {
+    title: "Booking Reference",
+    dataIndex: "bookingReference",
+    key: "bookingReference",
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Flight Details",
+    dataIndex: "flightDetails",
+    key: "flightDetails",
+  },
+  {
+    title: "Departure Time",
+    dataIndex: "departureTime",
+    key: "departureTime",
+  },
+  {
+    title: "Arrival Time",
+    dataIndex: "arrivalTime",
+    key: "arrivalTime",
+  },
+  {
+    title: "Passenger Names",
+    dataIndex: "passengerNames",
+    key: "passengerNames",
+    render: (text) => text.join(", "),
+  },
+  {
+    title: "Seat Numbers",
+    dataIndex: "seatNumbers",
+    key: "seatNumbers",
+    render: (text) => text.join(", "),
+  },
+  {
+    title: "Booking Status",
+    dataIndex: "bookingStatus",
+    key: "bookingStatus",
+  },
+  {
+    title: "Actions",
+    key: "actions",
+    render: (_, record) => (
+      <Space size="middle">
+        <a>Check-in Online</a>
+        <a>Modify Booking</a>
+      </Space>
+    ),
+  },
+];
+
+const UpcomingTripsTable = ({ upcomingTrips }) => (
+  <Table className={styles.container} columns={upcomingTripsColumns} dataSource={upcomingTrips} />
+);
+export default UpcomingTripsTable;
--- src/components/user/userRouter.js
+++ src/components/user/userRouter.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import UserNavbar from "./userNavbar/userNavbar";
 import UserTopBar from "./userTopbar/userTopBar";
 import UserAccountManagement from "./account/userAccountManagement";
+import UserDashboard from "./dashboard/userDashboard";
 
 const UserRouter = () => {
   return (
@@ -12,7 +13,7 @@ const UserRouter = () => {
           <UserNavbar>
             <UserTopBar />
             <Routes>
-              <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+              <Route path="/dashboard" element={<UserDashboard/>} />
               <Route path="/account" element={<UserAccountManagement/>} />
               <Route path="/flights" element={<h1>Flight</h1>} />
               <Route path="/reports" element={<h1>Report</h1>} />

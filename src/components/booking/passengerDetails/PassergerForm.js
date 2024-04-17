527db3e33fe461bdedf8aeb69db9c227dcefb2c9
Sunera Jayasinghe <106053595+sunerajayasinghe@users.noreply.github.com>
2024-04-17 12:38:10 +0530

    fix: use of dayjs instead of moment

--- src/components/booking/passengerDetails/PassergerForm.js
+++ src/components/booking/passengerDetails/PassergerForm.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Form, Input, Select, DatePicker, Card, Row, Col,Button,message } from 'antd';
-import { useNavigate,Link, useLocation } from 'react-router-dom';
+import { Form, Input, Select, DatePicker, Card, Row, Col, Button, message } from 'antd';
+import { useNavigate, Link, useLocation } from 'react-router-dom';
 import './PassengerForm.css';
 import axios from '../../../config/Axios';
-import moment from 'moment';
 import { useSelector } from "react-redux";
+import dayjs from "dayjs";
 
 const { Option } = Select;
 
@@ -13,17 +13,17 @@ function PassengerForm() {
     const navigate = useNavigate();
     const userEmail = useSelector(
         (state) => state.authenticationStateReducer.user?.email
-      );
+    );
 
-    
+
     const noOfPassengers = localStorage.getItem("passengerCount")
-    console.log('pasednpassenger no : ',noOfPassengers);
+    console.log('pasednpassenger no : ', noOfPassengers);
     const passengerNo = Array.from({ length: noOfPassengers }, (_, index) => index + 1);
 
     const [passengerDetails, setPassengerDetails] = useState([]);
     //validation
     const [formCompleted, setFormCompleted] = useState(false);
-    
+
     const seatType = "economy";
 
 
@@ -41,12 +41,12 @@ function PassengerForm() {
     //     noOfPassengers = value;
     //     passengerNo = Array.from({ length: noOfPassengers }, (_, index) => index + 1);
     // }
-    
+
 
     const [flight, setFlight] = useState([]);
 
     const handleClick = async () => {
-        console.log("Current date",passengerDetails);
+        console.log("Current date", passengerDetails);
 
         const isAnyPassengerIncomplete = passengerDetails.some(passenger => {
             return (
@@ -60,47 +60,47 @@ function PassengerForm() {
         });
 
         const isAnyPassengerunder18 = passengerDetails.some(passenger => {
-            let today = moment();
-            let birthDate = moment(passenger.dateOfBirth);
-            let age = today.diff(birthDate,'years');
-            console.log('age is : ',age);
+            let today = dayjs();
+            let birthDate = dayjs(passenger.dateOfBirth);
+            let age = today.diff(birthDate, 'years');
+            console.log('age is : ', age);
             return age < 18;
         })
-        
-        if(isAnyPassengerIncomplete){
+
+        if (isAnyPassengerIncomplete) {
             message.error('Please fill all the fields');
             return;
-        }if(isAnyPassengerunder18){
+        } if (isAnyPassengerunder18) {
             message.error('Passenger age should greater than 18');
             return;
         }
-        else{
-        
+        else {
+
             const bookingDetails = {
                 seatTypeBooked: seatType,
                 noOfSeatBooked: noOfPassengers,
                 flightId: 3,
                 passengers: passengerDetails,
-                userEmail:userEmail
+                userEmail: userEmail
             };
-            
-                
-                axios.post('http://localhost:8080/booking', bookingDetails) // Use Axios to send POST request
-            
+
+
+            axios.post('http://localhost:8080/booking', bookingDetails) // Use Axios to send POST request
+
                 .then(response => {
                     message.info('Submitted');
-                    
+
                     console.log('Passenger created:', response.data);
                     const bookingId = response.data.bookingId; // got booking id from back end
                     const flightId = response.data.flightId; // got flight id from backend
-                    console.log("Bookin id is :",bookingId);
-                    console.log("Flight id is :",flightId); 
+                    console.log("Bookin id is :", bookingId);
+                    console.log("Flight id is :", flightId);
 
                     const passingData = {
-                        totalPassengers : noOfPassengers,
-                        flightNo : flightId,
-                        seatType : seatType,
-                        bookingId : bookingId
+                        totalPassengers: noOfPassengers,
+                        flightNo: flightId,
+                        seatType: seatType,
+                        bookingId: bookingId
                     };
                     message.info('Passing  now');
                     localStorage.removeItem("passengerCount");
@@ -116,9 +116,9 @@ function PassengerForm() {
 
     const onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
-      };
+    };
 
-      //validation
+    //validation
     //   useEffect(() => {
     //     // Check if all form fields are filled
     //     const isFormComplete = passengerDetails.every(passenger => 
@@ -135,29 +135,29 @@ function PassengerForm() {
             <h2>Enter the passenger's details below</h2>
             {passengerNo.map((number, index) => (
                 <div key={index} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
-                    <Card bordered={true} style={{ width: '1500px', background: '#6E6E7F'}} >
+                    <Card bordered={true} style={{ width: '1500px', background: '#6E6E7F' }} >
                         <h3>Adult : {number}</h3>
 
                         <Form layout="vertical" onFinishFailed={onFinishFailed}>
                             <Row gutter={[16, 16]}>
                                 <Col span={8}>
-                                    <Form.Item  className ="bold-label" label="First Name" name="firstName" rules={[{ required: true, message: 'Please input your First Name!' }]}>
-                                        <Input onChange={(e) => handleFormChange(index, 'firstName', e.target.value)} placeholder="First Name"/>
+                                    <Form.Item className="bold-label" label="First Name" name="firstName" rules={[{ required: true, message: 'Please input your First Name!' }]}>
+                                        <Input onChange={(e) => handleFormChange(index, 'firstName', e.target.value)} placeholder="First Name" />
                                     </Form.Item>
                                 </Col>
                                 <Col span={8}>
                                     <Form.Item label="Last Name" name="lastName" rules={[{ required: true, message: 'Please input your Last Name!' }]}>
-                                        <Input onChange={(e) => handleFormChange(index, 'lastName' , e.target.value)}/>
+                                        <Input onChange={(e) => handleFormChange(index, 'lastName', e.target.value)} />
                                     </Form.Item>
                                 </Col>
                                 <Col span={8}>
                                     <Form.Item label="Passport No" name="passportNo" rules={[{ required: true, message: 'Please input your Passport No!' }]}>
-                                        <Input onChange = {(e) => handleFormChange(index,'passportNo',e.target.value)}/>
+                                        <Input onChange={(e) => handleFormChange(index, 'passportNo', e.target.value)} />
                                     </Form.Item>
                                 </Col>
                                 <Col span={8}>
-                                <Form.Item label="Meal Preference">
-                                        <Select placeholder="Select this" onChange={(value) => handleFormChange(index,'mealPreference', value)}>
+                                    <Form.Item label="Meal Preference">
+                                        <Select placeholder="Select this" onChange={(value) => handleFormChange(index, 'mealPreference', value)}>
                                             <Option value="veg">Vegetarian</Option>
                                             <Option value="nonVeg">Non-Vegetarian</Option>
                                         </Select>
@@ -165,12 +165,12 @@ function PassengerForm() {
                                 </Col>
                                 <Col span={8}>
                                     <Form.Item label="Date of Birth" >
-                                        <DatePicker minDate={moment().subtract(18,"years")} onChange={(date, dateString) => handleFormChange(index,'dateOfBirth', dateString)}/>
+                                        <DatePicker minDate={dayjs().subtract(18, "years")} onChange={(date, dateString) => handleFormChange(index, 'dateOfBirth', dateString)} />
                                     </Form.Item>
                                 </Col>
                                 <Col span={8}>
                                     <Form.Item label="Gender">
-                                        <Select placeholder="Select the gender" onChange={(value)=> handleFormChange(index, 'gender', value)}>
+                                        <Select placeholder="Select the gender" onChange={(value) => handleFormChange(index, 'gender', value)}>
                                             <Option value="male">Male</Option>
                                             <Option value="female">Female</Option>
                                             <Option value="other">Other</Option>
@@ -184,7 +184,7 @@ function PassengerForm() {
             ))}
             <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <Button type="primary" htmlType="submit" onClick={handleClick} danger style={{ width: '200px' }}>
-                        Submit 
+                    Submit
                 </Button>
             </div>
         </div>
